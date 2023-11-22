8ad96c1d2f9b49b31c5b11e09a8a971af2d4c4cf
Richard Fernandes Passarelli <45811737874@info.local>
2023-11-21 21:12:24 -0300

    Incluido Script do Banco De Dados

---
+++ query_Banc_bd_mamuka.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE bd_mamuka;
+
+USE bd_mamuka;
+
+
+CREATE TABLE tb_usuario(
+id BINARY(16) NOT NULL,
+nome VARCHAR(255) NOT NULL,
+cpf VARCHAR(255) NOT NULL UNIQUE,
+dataNascimento DATE,
+statusAtividade BOOL,
+departamento VARCHAR(255) NOT NULL,
+responsavel BINARY(16) NOT NULL,
+perfil VARCHAR(255) NOT NULL,
+cargo VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+senha VARCHAR(255) NOT NULL,
+id_tipoUsuario BINARY(16) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_tipoUsuario) REFERENCES tb_usuario(id)
+);
+
+CREATE TABLE tb_tipoUsuario (
+	id BINARY(16) NOT NULL,
+	tipoUsuario VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_projeto (
+id BINARY(16) NOT NULL,
+nomeProjeto VARCHAR(255) NOT NULL,
+statusProjeto VARCHAR(255) NOT NULL,
+dataInicio DATETIME,
+dataConclusao DATETIME,
+id_usuario BINARY(16),
+PRIMARY KEY (id),
+FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id)
+);
+
+CREATE TABLE tb_tarefa (
+id BINARY(16) NOT NULL,
+nomeTarefa VARCHAR(255) NOT NULL,
+statusTarefa VARCHAR(255) NOT NULL,
+dataInicio DATETIME,
+dataConclusao DATETIME,
+id_usuario BINARY(16),
+id_projeto BINARY(16) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_usuario) REFERENCES tb_tarefa (id),
+FOREIGN KEY (id_projeto) REFERENCES tb_projeto (id)
+);
+
